f92ee1b282bd861ab268c15f76b1e95dbb1d0ce6
BuildTools <unconfigured@null.spigotmc.org>
2017-03-25 13:58:25 -0400

    New Login Design

--- AleeOS/aleeos_login.cs
+++ AleeOS/aleeos_login.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace AleeOS
 {
-    public partial class Form1 : Form
+    public partial class aleeos_login : Form
     {
-        public Form1()
+        public aleeos_login()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
