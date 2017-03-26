8dac3b8471250cecd092c417f71b1eeed68f9036
Alee14 <alee14498@gmail.com>
2017-03-25 22:25:22 -0400

    Added Our Offical Site
    So yeah... I added the site but it doesn't exist yet the site but domain
    yes!

--- AleeOS/aleeos_login.Designer.cs
+++ AleeOS/aleeos_login.Designer.cs
@@ -34,13 +34,14 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Light", 22.25F);
-            this.label1.Location = new System.Drawing.Point(76, 25);
+            this.label1.Location = new System.Drawing.Point(89, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(279, 41);
             this.label1.TabIndex = 0;
@@ -49,7 +50,7 @@
             // Login
             // 
             this.Login.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Login.Location = new System.Drawing.Point(146, 157);
+            this.Login.Location = new System.Drawing.Point(146, 160);
             this.Login.Name = "Login";
             this.Login.Size = new System.Drawing.Size(126, 52);
             this.Login.TabIndex = 1;
@@ -61,7 +62,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 334);
+            this.label2.Location = new System.Drawing.Point(12, 362);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(99, 22);
             this.label2.TabIndex = 2;
@@ -70,7 +71,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold);
-            this.button1.Location = new System.Drawing.Point(146, 238);
+            this.button1.Location = new System.Drawing.Point(146, 249);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(126, 52);
             this.button1.TabIndex = 3;
@@ -81,7 +82,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Symbol", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(73, 106);
+            this.label3.Location = new System.Drawing.Point(75, 112);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(293, 26);
             this.label3.TabIndex = 4;
@@ -92,17 +93,28 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Lucida Sans", 14.25F);
-            this.label4.Location = new System.Drawing.Point(234, 334);
+            this.label4.Location = new System.Drawing.Point(252, 362);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(183, 22);
             this.label4.TabIndex = 5;
             this.label4.Text = "Made By: AleeCorp";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Sans", 14.25F);
+            this.label5.Location = new System.Drawing.Point(74, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(314, 22);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Our Offical Site: http://aleeos.ga";
+            // 
             // aleeos_login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(429, 371);
+            this.ClientSize = new System.Drawing.Size(445, 393);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
@@ -125,6 +137,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
